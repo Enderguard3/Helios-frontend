e0e48b1601eca791f2bb807da0a1fd7c59363e14
César Ombredane <combredane@gmail.com>
2021-06-12 18:16:48 +0200

    add comments to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import {useState} from "react"
 import {useHistory} from "react-router-dom"
 import {AppBar, Toolbar, Typography, IconButton, Divider, List, ListItem, ListItemIcon, ListItemText, Drawer, makeStyles} from '@material-ui/core'
-import {Home, CommentOutlined, AccountCircle, InsertChart, Menu, ArrowLeft, Announcement} from '@material-ui/icons'
+import {Home, CommentOutlined, AccountCircle, InsertChart, Menu, ArrowLeft, Announcement, ChatBubble} from '@material-ui/icons'
 
 const style = makeStyles(theme => ({
     menu: {
@@ -61,6 +61,7 @@ const Navbar = ({name}) => {
                             {name: "Rating", icon: <CommentOutlined />},
                             {name: "Leaderboard", icon: <InsertChart />},
                             {name: "News", icon: <Announcement />},
+                            {name: "Comments", icon: <ChatBubble />},
                             {name: "Test", icon: null},
                         ].map(({name, icon}) => (
                             <ListItem button key={name} onClick={() => handleClickPage(name)}>
